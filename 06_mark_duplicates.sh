1a4ecf30ef9b5966f6e5ed4cf93273816227dfa3
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2021-05-07 17:16:24 +0930

    Create 06_mark_duplicates.sh

---
+++ 06_mark_duplicates.sh
@@ -0,0 +1,18 @@
+cd /hpcfs/users/a1717363/IncaModern/
+
+merge=./04-mergebam
+markdup=./05-markdup
+samples=(Ka24 K24 K29 PUN67 PUN68 PUN76)
+parallel=6
+
+for (( i=0 ; i<${#samples[@]} ; i += $parallel ))
+do
+    for j in $(seq $parallel)
+    do
+    individ=${samples[$i + $j - 1]}
+    if [[ -z $individ ]] ; then break 2; fi
+    bammarkduplicates2 index=1 markthread=8 I=$merge/${individ}_merged.bam O=$markdup/${individ}_markdup.bam &
+    pids[$j]=$!
+    done
+    for pid in ${pids[@]}; do wait $pid; done
+done
