98bc35a4f16de008b075d872658f4a4412564ae8
roberta-davidson <roberta.davidson@adelaide.edu.au>
2024-03-04 17:10:57 +1030

    annotate 01 and 02

--- 01_Dumultiplexing_bcl2fastq.sh
+++ 01_Dumultiplexing_bcl2fastq.sh
@@ -1,6 +1,10 @@
+#!/bun/bash
+
+#load modules
 module load arch/haswell 
 module load bcl2fastq2/2.19.1
 
+#use BCL2fastq to convert bcl files from sequencer to fastq
 bcl2fastq \
 --runfolder-dir <path_to_directory_containing_bcl files> \
 --output-dir <path_to_output_directory_for_fastq_files \
--- 02_fastp.sh
+++ 02_fastp.sh
@@ -1,4 +1,6 @@
-cd /hpcfs/users/aXXXXXX/IncaModern/Lane_6/PUN76/
+#!/bin/bash
+
+#fastp script for one sample 
 
 fastp --verbose \
 --thread 16 \
@@ -7,3 +9,9 @@ fastp --verbose \
 --in2 ./PUN76_S1_L006_R2_001.fastq.gz \ 
 --out1 ./PUN76_R1.fastp.fastq.gz \
 --out2 ./PUN76_R2.fastp.fastq.gz
+
+#flags explained by chatgpt:
+#    -g or --cut_right: This option specifies the number of bases to trim from the 3' (right) end of each read. It is used to remove low-quality bases from the end of reads.
+#    -x or --cut_tail: This option specifies the minimum length threshold for reads after trimming. Any reads shorter than this threshold after trimming will be discarded.
+#    -c or --correction: This option enables base correction. Fastp will perform error correction on the input reads, attempting to fix base errors.
+#    -h or --html: This option specifies the output HTML report file name. Fastp generates an HTML report summarizing the trimming and filtering statistics, which can be useful for visualizing the quality control results.
