2a2cb4b870b3f02205c42285c50f63822ce5d713
roberta-davidson <roberta.davidson@adelaide.edu.au>
2024-03-07 11:23:26 +1030

    linting

--- 02_fastp.sh
+++ 02_fastp.sh
@@ -6,7 +6,7 @@ fastp --verbose \
 --thread 16 \
 -g -x -c -h PUN76.fastp.html -j PUN76_fastp.json \
 --in1 ./PUN76_S1_L006_R1_001.fastq.gz \
---in2 ./PUN76_S1_L006_R2_001.fastq.gz \ 
+--in2 ./PUN76_S1_L006_R2_001.fastq.gz \
 --out1 ./PUN76_R1.fastp.fastq.gz \
 --out2 ./PUN76_R2.fastp.fastq.gz
 
